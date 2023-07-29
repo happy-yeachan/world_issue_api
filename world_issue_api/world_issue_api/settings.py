c168f38d9dbb5ef38196e9330406cb2311ea2bc6
yeachan <dus001228@naver.com>
2023-07-29 20:24:35 +0900

    cors 취소

--- world_issue_api/world_issue_api/settings.py
+++ world_issue_api/world_issue_api/settings.py
@@ -123,8 +123,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-CORS_ALLOWED_ORIGINS = [
-	'*',
-]
