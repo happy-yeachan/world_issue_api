cf397f91b8cd10f165b1a38a073ade3e9ab76dfa
yeachan <dus001228@naver.com>
2023-07-30 20:02:07 +0900

    날짜 자동 최신화

--- world_issue_api/issue_api/models.py
+++ world_issue_api/issue_api/models.py
@@ -6,7 +6,7 @@ class Issue(models.Model):
     url = models.URLField() # 주소
     img = models.CharField(max_length=9999) # 제목
     visite_count = models.IntegerField(default=0)
-    created_at = models.DateField()
+    created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
