fa2b63eb00b29d21a81d14fcb26bd2a87d8a7d88
jckim22 <jckim229@gmail.com>
2023-08-03 01:59:16 +0900

    코드 함수화#4

--- world_issue_api/func.py
+++ world_issue_api/func.py
@@ -2,12 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 from papago import *
 import re
-def news_scraping(url_address,second_url_address,title_path,img_path,url_path,country_name,is_korea):
+
+def news_scraping(url_address,second_url_address,title_path,img_path,url_path,country_name,is_korea,content_path):
     try:
         url = url_address
         page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
-
         title = soup.select_one(title_path).text
         img = soup.select_one(img_path)['src']
         if country_name=='영국' or country_name=='베트남':
@@ -18,14 +18,28 @@ def news_scraping(url_address,second_url_address,title_path,img_path,url_path,co
         title = re.sub(r"^\s+|\s+$", "", title)
         if not is_korea:
             title = get_translate(str(title))
-
         result = {
             'country': country_name,
             'title': title,
             'img': img,
-            'url': second_url_address + url
+            'url': second_url_address + url,        
         }
+        #url 주소가 덧붙여서 나오는 나라들임, 그 때 그 떄 마다 다를 수 있어서 수정필요
+        if country_name=='인도' or country_name=='프랑스' or country_name=='이탈리아' or country_name=='대한민국' or country_name=='영국' or country_name=='베트남':
+            result['url']=url
+        print(result['url'])
+        content=content_scraping(result['url'],content_path)
+        result['content']=content
         return result
-    except:
+    except Exception as ex:            
         print('error:',end='')
-        print(country_name)
+        print(country_name)    
+        print(ex)
+
+def content_scraping(url,content_path):
+    
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')    
+    content = soup.select_one(content_path).text
+    print(content)
+    return content
