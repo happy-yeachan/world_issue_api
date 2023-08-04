aefe11ae3e572232cbc85f7cc9f5f92b1c7bc178
yeachan <dus001228@naver.com>
2023-08-04 14:31:18 +0900

    visit

--- world_issue_api/issue_api/models.py
+++ world_issue_api/issue_api/models.py
@@ -6,7 +6,7 @@ class Issue(models.Model):
     content = models.TextField()
     url = models.URLField() # 주소
     img = models.CharField(max_length=9999) # 제목
-    visite_count = models.IntegerField(default=0)
+    visit_count = models.IntegerField(default=0)
     created_at = models.CharField(max_length=30)
 
     def __str__(self):
