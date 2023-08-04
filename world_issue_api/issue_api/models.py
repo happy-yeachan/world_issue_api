3087926fa01dc517df7b2ae8d15c571c49754d73
yeachan <dus001228@naver.com>
2023-08-04 14:48:13 +0900

    created_at null true

--- world_issue_api/issue_api/models.py
+++ world_issue_api/issue_api/models.py
@@ -7,7 +7,7 @@ class Issue(models.Model):
     url = models.URLField() # 주소
     img = models.CharField(max_length=9999) # 제목
     visit_count = models.IntegerField(default=0)
-    created_at = models.CharField(max_length=30)
+    created_at = models.CharField(max_length=30, null=True)
 
     def __str__(self):
         return self.title
