08b5dab0128104fb0cd6593d1478b885f8d5c9e3
yeachan <dus001228@naver.com>
2023-08-10 11:24:22 +0900

    (feat) - visit_count post요청으로 받아 처리

--- world_issue_api/issue_api/views.py
+++ world_issue_api/issue_api/views.py
@@ -1,7 +1,16 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .serializers import IssueSerializer
 from .models import Issue
 
 class IssueViewSet(viewsets.ModelViewSet):
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
+
+    @action(detail=True, methods=['post'])
+    def increment_visit_count(self, request, pk=None):
+        issue = self.get_object()
+        issue.visit_count += 1
+        issue.save()
+        return Response({'message': str(pk) +'방문'})
